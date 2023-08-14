6eb795ff3f99d6554922382c884e94931ab6b460
Roberto Ochoa <oc_005@hotmail.com>
2023-08-14 11:58:00 -0300

    forgot password issue

--- app/[locale]/admin/layout.js
+++ app/[locale]/admin/layout.js
@@ -2,22 +2,13 @@
 import * as React from "react";
 import AdminNavigation from "../components/AdminNavigation/AdminNavigation";
 require("dotenv").config();
-
+import SessionProviders from "../../sessionProvider";
 
 export default function Admin({ children }) {
   return (
-    <html lang="en">
-    <body>
-      <section>
-        <AdminNavigation/>
-        
-      {children}
-      </section>
-      </body>
-  </html>
+        <section>
+          <AdminNavigation />
+          {children}
+        </section>
   );
 }
-
-{/* <section>
-{children}
-</section> */}
