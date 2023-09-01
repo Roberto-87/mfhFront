dc6523855dba98d4b4f37d5119c484e0226f2c5c
Roberto Ochoa <oc_005@hotmail.com>
2023-09-01 19:54:11 -0300

    firebase deploy

--- app/firebase/firebase.js
+++ app/firebase/firebase.js
@@ -16,7 +16,7 @@ const firebaseConfig = {
 };
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
-export { app, analytics };
+/* const analytics = getAnalytics(app); */
+const auth= getAuth()
+export { app, auth };
 
