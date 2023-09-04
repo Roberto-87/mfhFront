9f3a1116b800554e06c3d0c6cccb9eec3ba9ff60
Roberto Ochoa <oc_005@hotmail.com>
2023-09-04 14:54:02 -0300

    exhibition iasrihfasdidsagFunction Key

--- app/[locale]/components/ExhibitionLink/ExhibitionLink.jsx
+++ app/[locale]/components/ExhibitionLink/ExhibitionLink.jsx
@@ -4,11 +4,11 @@ import styleExhibition from '../CardExhibitions/CardExhibition.module.css'
 const ExhibitionLink=({exhibition})=>{
     return(
         <Link  href={`/exhibitions/${(exhibition?.id)}`}>
-        { exhibition.images.map((each, index)=>
+{/*         { exhibition.images.map((each, index)=> index< 1 && */}
           <div key={index} style={{display:'flex',justifyContent:'center'}}>
-            <img style={{width:'80%'}} alt='imagen exhibicion' className={styleExhibition.cardImageExhibition} key={index} src={each.replace('http','https')} srcSet={each.replace('http','https')} /> 
+            <img style={{width:'80%'}} alt='imagen exhibicion' className={styleExhibition.cardImageExhibition} key={index} src={each.replace('http','https')} srcSet={exhibition.images[0].replace('http','https')} /> 
               </div>
-        )}
+{/*         )} */}
       </Link>
     )
 }
