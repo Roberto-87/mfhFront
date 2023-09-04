70c2d11975a16c5dc23ccba5c86437ae0d2dc8a7
Roberto Ochoa <oc_005@hotmail.com>
2023-09-04 14:12:50 -0300

    text id http

--- app/[locale]/components/TextDetailCard/TextDetailCard.jsx
+++ app/[locale]/components/TextDetailCard/TextDetailCard.jsx
@@ -9,7 +9,7 @@ const TextDetailCard=({paper})=>{
   </Box>
 
     {paper.format === 'pdf' ?
-      <embed src={paper.image} style={{width:'65%',height:'100%'}} alt={paper.title}></embed>
+      <embed src={paper.image.replace('http','https')} style={{width:'65%',height:'100%'}} alt={paper.title}></embed>
     :
     <img src={paper.image} style={{width:'35%'}} alt={paper.title} />
     }
