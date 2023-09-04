cddf7f7719c287ea1ae3cd2af24c931fa9f740b7
Roberto Ochoa <oc_005@hotmail.com>
2023-09-04 13:53:46 -0300

    exhibition fixed

--- app/[locale]/components/ExhibitionLink/ExhibitionLink.jsx
+++ app/[locale]/components/ExhibitionLink/ExhibitionLink.jsx
@@ -6,8 +6,8 @@ const ExhibitionLink=({exhibition})=>{
         <Link  href={`/exhibitions/${(exhibition?.id)}`}>
         { exhibition.images.map((each, index)=> index< 1 &&
           <div key={index} style={{display:'flex',justifyContent:'center'}}>
-            <img style={{width:'80%'}} alt='imagen exhibicion' className={styleExhibition.cardImageExhibition} key={index} src={each} /> 
-              </div>
+          {/*   <img style={{width:'80%'}} alt='imagen exhibicion' className={styleExhibition.cardImageExhibition} key={index} src={each} /> 
+              */} </div>
         )}
       </Link>
     )
