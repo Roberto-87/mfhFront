53be87787b64cd9439f75dfaa207570c15d7c999
Roberto Ochoa <oc_005@hotmail.com>
2023-09-04 13:21:12 -0300

    exhibition image

--- app/[locale]/components/ExhibitionLink/ExhibitionLink.jsx
+++ app/[locale]/components/ExhibitionLink/ExhibitionLink.jsx
@@ -4,7 +4,7 @@ import styleExhibition from '../CardExhibitions/CardExhibition.module.css'
 const ExhibitionLink=({exhibition})=>{
     return(
         <Link  href={`/exhibitions/${(exhibition?.id)}`}>
-        { exhibition.images.map((each, index)=> index< 1 &&
+        { exhibition.map((each, index)=> index< 1 &&
           <div key={index} style={{display:'flex',justifyContent:'center'}}>
             <img style={{width:'80%'}} alt='imagen exhibicion' className={styleExhibition.cardImageExhibition} key={index} src={each} /> 
               </div>
