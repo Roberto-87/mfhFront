9651f1fc8f7a24d2020fbabe14ee05e86138db2d
Roberto Ochoa <oc_005@hotmail.com>
2023-09-04 20:54:48 -0300

    fix bug

--- app/[locale]/components/ExhibitionLink/ExhibitionLink.jsx
+++ app/[locale]/components/ExhibitionLink/ExhibitionLink.jsx
@@ -2,12 +2,11 @@ import Link from "next/link"
 import styleExhibition from '../CardExhibitions/CardExhibition.module.css'
 
 const ExhibitionLink=({exhibition})=>{
-  usE
     return(
         <Link  href={`/exhibitions/${(exhibition?.id)}`}>
         { exhibition.images.map((each, index)=> index< 1 &&
           <div key={index} style={{display:'flex',justifyContent:'center'}}>
-            <img style={{width:'80%'}} alt='imagen exhibicion' className={styleExhibition.cardImageExhibition} key={index} src={each.replace('http','https')} srcSet={each.replace('http','https')} /> 
+            <img style={{width:'80%'}} alt='imagen exhibicion' className={styleExhibition.cardImageExhibition} key={index} src={each.replace('http','https')} /> 
               </div>
         )}
       </Link>
