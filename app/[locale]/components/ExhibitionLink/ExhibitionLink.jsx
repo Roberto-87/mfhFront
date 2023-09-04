1e5e98258eed47b748b487dba31aced6419007e9
Roberto Ochoa <oc_005@hotmail.com>
2023-09-04 19:50:30 -0300

    error iexhibition

--- app/[locale]/components/ExhibitionLink/ExhibitionLink.jsx
+++ app/[locale]/components/ExhibitionLink/ExhibitionLink.jsx
@@ -2,9 +2,9 @@ import Link from "next/link"
 import styleExhibition from '../CardExhibitions/CardExhibition.module.css'
 
 const ExhibitionLink=({exhibition})=>{
-  
+  usE
     return(
-        <Link  href={`/exhibitions/category/${(exhibition?.id)}`}>
+        <Link  href={`/exhibitions/${(exhibition?.id)}`}>
         { exhibition.images.map((each, index)=> index< 1 &&
           <div key={index} style={{display:'flex',justifyContent:'center'}}>
             <img style={{width:'80%'}} alt='imagen exhibicion' className={styleExhibition.cardImageExhibition} key={index} src={each.replace('http','https')} srcSet={each.replace('http','https')} /> 
