8ffeba0fbb58fc51d14ec2e2cdbe7bd7d6e5acf2
Roberto Ochoa <oc_005@hotmail.com>
2023-09-05 19:17:02 -0300

    text style

--- app/[locale]/components/TextDetailCard/TextDetailCard.jsx
+++ app/[locale]/components/TextDetailCard/TextDetailCard.jsx
@@ -6,13 +6,12 @@ const TextDetailCard=({paper})=>{
  return(
   <>
   {paper.format === 'pdf' ?
-<Box sx={styleTextDetail} className={style.containerDocument} >
-    <embed src={paper.image.replace('http','https')} style={{width:'65%',height:'100%'}} alt={paper.title}></embed>
-    </Box>     
+
+  <embed src={paper.image.replace('http','https')} style={{width:'65%',height:'100%'}} alt={paper.title}></embed>
+
   :
-  <Box sx={styleTextDetail} className={style.containerDocument} >
   <img src={paper.image} style={{width:'35%'}} alt={paper.title} />
-  </Box> 
+
   }
   
   </>
