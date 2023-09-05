a1a3315a4be86843e4a48b252959be1400152e90
Roberto Ochoa <oc_005@hotmail.com>
2023-09-05 19:24:31 -0300

    paper style

--- app/[locale]/components/TextDetailCard/TextDetailCard.jsx
+++ app/[locale]/components/TextDetailCard/TextDetailCard.jsx
@@ -3,17 +3,20 @@ import {styleTextDetail} from './itemStyle'
 import style from './TextDetailCard.module.css'
 
 const TextDetailCard=({paper})=>{
- return(
- <Box sx={styleTextDetail} className={style.containerDocument} >
-    <Box sx={{display:'flex', flexDirection:'column', justifyContent:'center',color:'black'}}>
-  </Box>
 
-    {paper.format === 'pdf' ?
-      <embed src={paper.image.replace('http','https')} style={{width:'65%',height:'100%'}} alt={paper.title}></embed>
-    :
-    <img src={paper.image} style={{width:'35%'}} alt={paper.title} />
-    }
- </Box>     
- )
+  if(paper.image){
+    return(
+   <Box sx={styleTextDetail} className={style.containerDocument} >
+      <Box sx={{display:'flex', flexDirection:'column', justifyContent:'center',color:'black'}}>
+    </Box>
+  
+      {paper.format === 'pdf' ?
+        <embed src={paper.image.replace('http','https')} style={{width:'65%',height:'100%'}} alt={paper.title}></embed>
+      :
+      <img src={paper.image} style={{width:'35%'}} alt={paper.title} />
+      }
+   </Box>     
+   )
+  }
 }
 export default TextDetailCard
