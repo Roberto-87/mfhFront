7b9303b1bb5a0d1400f712fb197bce6311fc1b1f
Roberto Ochoa <oc_005@hotmail.com>
2023-09-05 19:08:43 -0300

    text style

--- app/[locale]/components/TextDetailCard/TextDetailCard.jsx
+++ app/[locale]/components/TextDetailCard/TextDetailCard.jsx
@@ -6,14 +6,14 @@ const TextDetailCard=({paper})=>{
  return(
  <Box sx={styleTextDetail} className={style.containerDocument} >
     <Box sx={{display:'flex', flexDirection:'column', justifyContent:'center',color:'black'}}>
-  </Box>
 
     {paper.format === 'pdf' ?
       <embed src={paper.image.replace('http','https')} style={{width:'65%',height:'100%'}} alt={paper.title}></embed>
-    :
-    <img src={paper.image} style={{width:'35%'}} alt={paper.title} />
+      :
+      <img src={paper.image} style={{width:'35%'}} alt={paper.title} />
     }
  </Box>     
+    </Box>
  )
 }
 export default TextDetailCard
