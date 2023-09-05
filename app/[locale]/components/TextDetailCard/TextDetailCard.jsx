fdae2b8b8f3acc6932732766c5e26d0eee90cca7
Roberto Ochoa <oc_005@hotmail.com>
2023-09-05 18:24:09 -0300

    text style

--- app/[locale]/components/TextDetailCard/TextDetailCard.jsx
+++ app/[locale]/components/TextDetailCard/TextDetailCard.jsx
@@ -1,13 +1,16 @@
 import { Box } from "@mui/material"
 import {styleTextDetail} from './itemStyle'
 import style from './TextDetailCard.module.css'
+import { imageFormat } from "../../../utils/functions"
 
 const TextDetailCard=({paper})=>{
  return(
  <Box sx={styleTextDetail} className={style.containerDocument} >
-    <Box sx={{display:'flex', flexDirection:'column', justifyContent:'center',color:'black'}}>
-  </Box>
-   <embed src={paper.image.replace('http','https')} style={{width:'65%',height:'100%'}} alt={paper.title}></embed>
+    {paper.format === 'pdf' ?
+      <embed src={imageFormat(paper.image)} style={{width:'65%',height:'100%'}} alt={paper.title}></embed>
+    :
+    <img src={imageFormat(paper.image)} style={{width:'35%'}} alt={paper.title} />
+    }
  </Box>     
  )
 }
