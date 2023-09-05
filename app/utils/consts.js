27c62a5bab2cc11f480ef041c5c068bc1f16f37f
Roberto Ochoa <oc_005@hotmail.com>
2023-09-05 11:30:49 -0300

    const exhibitions

--- app/utils/consts.js
+++ app/utils/consts.js
@@ -1,10 +1,10 @@
-//export const BASE_URL= 'http://localhost:3001/' 
-export const BASE_URL='https://mfh-backend.onrender.com/'
+export const BASE_URL= 'http://localhost:3001/' 
+/* export const BASE_URL='https://mfh-backend.onrender.com/' */
 export const PORTFOLIO= 'portfolio'
 export const WORK= 'work'
 export const TEXT= 'text'
 export const BIO= 'bio'
 export const COVER= 'cover'
-export const EXHIBITIONSGET= 'exhibitions'
+export const EXHIBITIONS= 'exhibitions'
 export const WORKS= 'works'
 export const CONTACT= 'contact'
