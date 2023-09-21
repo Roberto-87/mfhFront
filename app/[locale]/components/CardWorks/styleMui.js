1e3ed47fba27d630bcb7bdb7611b1a790e8d010f
Roberto Ochoa <oc_005@hotmail.com>
2023-09-21 16:33:19 -0300

    modal curatorial

--- app/[locale]/components/CardWorks/styleMui.js
+++ app/[locale]/components/CardWorks/styleMui.js
@@ -2,7 +2,7 @@ import { styled } from '@mui/material/styles';
 import Paper from '@mui/material/Paper';
 
 export const Item = styled(Paper)(({ theme }) => ({
-    backgroundColor: 'transparent',
+  /*   backgroundColor: 'transparent', */
     ...theme.typography.body2,
     padding: theme.spacing(2),
     textAlign: 'center',
