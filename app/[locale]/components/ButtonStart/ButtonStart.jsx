cb9a55804eea74f06d2d2c8637ea7245d46c9c0a
Roberto Ochoa <oc_005@hotmail.com>
2023-09-22 17:44:28 -0300

    change button inicio per start

--- app/[locale]/components/ButtonStart/ButtonStart.jsx
+++ app/[locale]/components/ButtonStart/ButtonStart.jsx
@@ -5,7 +5,7 @@ import Link from "next/link"
 const ButtonStart=()=>{
     return (
         <div className={style.containerStartButton}>
-             <Link style={{width:'10%'}} className={style.linkStartButton} locale="en" href={`works`}><Button variant="outlined"  sx={{ color: 'gray', backgroundColor: 'transparent', borderColor: 'gray', padding:'6%' }} >Inicio</Button></Link>
+             <Link style={{width:'10%'}} className={style.linkStartButton} locale="en" href={`works`}><Button variant="outlined"  sx={{ color: 'gray', backgroundColor: 'transparent', borderColor: 'gray', padding:'6%' }} >Start</Button></Link>
 {/*             <Link locale="en" href={`works`}><Button variant="outlined"  sx={{ color: 'black', backgroundColor: 'transparent', borderColor: 'black' }} >EN</Button></Link>
             <Link locale="es" href={'works'}><Button variant="outlined"  sx={{ color: 'black', backgroundColor: 'transparent', borderColor: 'black' }} >ES</Button></Link> */}
         </div>
