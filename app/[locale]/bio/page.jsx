8844b992159c0199fde1e449df9dea9bc46f2a06
Roberto Ochoa <oc_005@hotmail.com>
2023-09-22 17:34:25 -0300

    bio layout refresh

--- app/[locale]/bio/page.jsx
+++ app/[locale]/bio/page.jsx
@@ -1,9 +1,8 @@
 'use client'
 import { useState, useEffect } from 'react';
 import getData from '../hooks/getData'
-import { Box } from "@mui/material";
+import { Box, Paper } from "@mui/material";
 import Grid from '@mui/material/Grid';
-import { Item } from './itemStyle';
 import { BIO } from '../../utils/consts';
 import {comfortaa} from '../fonts/fonts'
 import LoadingBar from 'react-top-loading-bar'
@@ -11,7 +10,16 @@ import { LazyLoadImage } from "react-lazy-load-image-component";
 import 'react-lazy-load-image-component/src/effects/opacity.css'
 import LoaderAnimation from '../components/LoaderAnimation/LoaderAnimation'
 import Footer from '../components/Footer/Footer';
+import { styled } from '@mui/material/styles';
 
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'start',
+  boxShadow:'none',
+  color: theme.palette.text.secondary,
+}));
 
 const Bio = () => {
   const [progress, setProgress] = useState(0)
@@ -32,54 +40,49 @@ useEffect(() => {
 } catch (error) {
   return {error:error.message}
 }
- 
- 
     }, []);
     const onHandleLoad=()=>{
       setImageLoaded(true)
     }
 
-    return (< main style={{ paddingBottom:'25px'}}>    
-      <Box sx={{ flexGrow: 1, marginTop:'2%'}}>
-      <LoadingBar  color='black'progress={progress}  />
-      {!bio && <div style={{top:'5%',left:'50%' , height:'30vh', position:'fixed',   alignItems: 'center',display:'flex', justifyContent:'center'
-}}>   <LoaderAnimation   />
-      </div>
-   }
-  {bio && bio?.map(({title, image, subtitle, text, id})=> 
-  <>
- 
- <Grid item xs={12} sm={6} md={4} lg={3} gap={2} sx={{border: 'none', color: 'black'}}>
-    <div style={{display: 'flex', justifyContent: 'center'}}>
-      <h2 className={comfortaa.className}>{title}</h2>
-      <p className={comfortaa.className}>{subtitle}</p>
-    </div>
-  </Grid>
-  <Grid container spacing={2} style={{display: 'flex', justifyContent: 'center', border: 'none'}}>
-    <Grid item xs={12} sm={12} md={8} lg={9} gap={2} sx={{border: 'none', color: 'black'}}>
-      <Item style={{backgroundColor: 'white' }}>
-      {image && <LazyLoadImage loading='lazy' onLoad={onHandleLoad} effect='opacity' style={{width:'30%'}} src={image} alt={title}/>}
-      <p className={comfortaa.className}>{text.split('\n').map((line, index) => (
-  <div key={index} style={{display:'flex', justifyContent:'flex-start' , marginLeft:'0', paddingLeft:'4%'}}>
-      {line.match(yearRegex) ? <p style={{marginTop:'0', marginBottom:'0'}}>{line}</p>:  
-      <p style={{marginLeft:'4.2%', marginTop:'0', marginBottom:'0' }}>{line}</p>
-      }
-    <br />
-  </div>
-))}</p>
-      </Item>
-    </Grid>
-  </Grid>
-</>
-    
+    return (
+      <Box sx={{ flexGrow: 1 }}>
 
-     )
-   }
- </Box>
-   {bio && <Box  sx={{width:'100%', display:'flex',justifyContent:'flex-start', alignItems:'center', position:'fixed', bottom:'4%', marginLeft:'5px' }}>
-     <Footer   />
-   </Box> }
-</main>
-        )
+ <LoadingBar  color='black'progress={progress}  />
+    {!bio && 
+     <div style={{top:'5%',left:'50%' , height:'30vh', position:'fixed',   alignItems: 'center',display:'flex', justifyContent:'center'}}>
+        <LoaderAnimation/>
+    </div>
+    }    
+     
+    <Grid container spacing={2}   sx={{marginTop:'1%'}} >
+      <Grid xs={2}></Grid>
+         <Grid xs={9} rowSpacing={1} >
+    {bio && bio?.map(({title, image, subtitle, text, id})=> 
+            <Item key={id}>
+              <div style={{display:'flex', justifyContent:'center'}}>
+             <h2 className={comfortaa.className}>{title}</h2>
+              </div>
+             <p className={comfortaa.className}>{subtitle}</p>
+   {image && <LazyLoadImage loading='lazy' onLoad={onHandleLoad} effect='opacity' style={{width:'30%'}} src={image} alt={title}/>}
+            <p className={comfortaa.className}>{text.split('\n').map((line, index) => (
+          <div key={index} style={{display:'flex', justifyContent:'flex-start' , marginLeft:'0', paddingLeft:'4%'}}>
+          {line.match(yearRegex)  ? <p style={{marginTop:'0', marginBottom:'0', paddingTop:'1px'}}>{line}</p>:  
+            <p style={{  marginTop:'0', marginBottom:'0', paddingTop:'1px' }}>{line}</p>
+          }
+          <br />
+          </div>
+          ))}</p>  
+            </Item>
+  )}        
+          </Grid><Grid xs={1}>
+          </Grid>
+        </Grid> 
+          {bio && <Box  sx={{width:'100%', display:'flex',justifyContent:'flex-start', alignItems:'center', position:'fixed', bottom:'4%', marginLeft:'5px' }}>
+<Footer   />
+</Box> }
+      </Box>
+    );
+
 }
 export default Bio
